ad554bdbd5e6297aa29d34207c6b05658037fbdb
canisminor1990 <i@canisminor.cc>
2018-10-09 21:59:25 +0800

    update

--- database/resume.js
+++ database/resume.js
@@ -49,7 +49,7 @@ module.exports = {
         title: "User Experience Expert",
         time : ["2018.10 - 至今，", "体验设计专家"],
         desc :
-          "入职(ing)蚂蚁金服，财富事业群，|蚂蚁财富相关及组件化设计工作。"
+          "就职阿里巴巴蚂蚁金服AFUX，|财富事业群-用户产品体验部-设计中心，|蚂蚁财富相关及组件化设计工作。"
       },
       {
         img  : "/img/resume-experience-xmjk.png",
