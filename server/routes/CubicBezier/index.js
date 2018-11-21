91263add8b67a3a1d44481997dbb047a1e9ac4d0
YufanYang <yufan.yyf@alibaba-inc.com>
2018-11-21 18:29:00 +0800

    update

--- server/routes/CubicBezier/index.js
+++ server/routes/CubicBezier/index.js
@@ -1,10 +1,11 @@
 const CubicBezier = require('./CubicBezier');
 
 module.exports = ({ a, b, c, d, length = 1000 }) => {
+  length = parseInt(length);
   const CB = new CubicBezier([parseFloat(a), parseFloat(b)], [parseFloat(c), parseFloat(d)]);
   const result = [0];
   for (let i = 1; i < length + 1; i++) {
-    result.push(parseFloat(CB.getY(i / length).toFixed(3)));
+    result.push(parseFloat(CB.getY(i / length)).toFixed(3));
   }
   return result;
 };
